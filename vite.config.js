54f6bea076b8eedc790c68cdcaa6cac5c5785d49
promaa <101043703+promaaa@users.noreply.github.com>
2025-02-20 21:22:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
+import { words } from "./words"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [
-		react()
-	]
+  plugins: [react()],
+  build: {
+    outDir: 'build/client'
+  }
 })
+export function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length)
+    return words[randomIndex]
+}
+
+export function getFarewellText(language) {
+const options = [
+    `Adiós, ${language}`, 
+    `Descanse en paz, ${language}`,
+    `Te extrañaremos, ${language}`,
+    `¡Oh no, ¡no ${language}!`,
+    `${language} muerde el polvo`,
+    `Se fue pero no olvidado, ${language}`,
+    `El fin de ${language} tal como lo conocemos`,
+    `Hacia el atardecer, ${language}`,
+    `${language}, ha sido real`,
+    `${language}, tu vigilancia ha terminado`,
+    `${language} ha abandonado el edificio`
+];
+
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
